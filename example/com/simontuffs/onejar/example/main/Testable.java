1881daf05dcaec1f64e8521e203e68f41b8c707b
P. Simon Tuffs <simontuffs@users.sf.net>
2010-06-08 19:17:11 +0000

    Extend TestAble to support test invocation outside JUnit.

--- example/com/simontuffs/onejar/example/main/Testable.java
+++ example/com/simontuffs/onejar/example/main/Testable.java
@@ -1,5 +1,7 @@
 package com.simontuffs.onejar.example.main;
 
+import java.lang.reflect.Method;
+
 /**
  * This class is used to assist with JUnit testing.  It lets the one-jar-example be built
  * without any dependency on JUnit. 
@@ -18,6 +20,18 @@ public class Testable {
         System.out.println("* fail: " + reason);
         System.out.println("******************************************************************************************************************");
         failures++;
+    }
+    
+    public void runTests() throws Exception {
+        Method methods[] = this.getClass().getMethods();
+        for (int i=0; i<methods.length; i++) {
+            Method method = methods[i];
+            if (!method.getName().equals("runTests") && method.getName().startsWith("test")) {
+                System.out.println("testing: " + method);
+                method.invoke(this, null);
+                System.out.println("------------------------------------------------------------------------------------------------------------------");
+            }
+        }
     }
 
 }
