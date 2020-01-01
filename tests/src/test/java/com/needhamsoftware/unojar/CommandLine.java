45c2e79770b5cac690a27683d1683e78ea3212c6
nsoft <gus@needhamsoftware.com>
2020-01-01 17:37:42 -0500

    Global reformat to 2 spaces

--- tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
+++ tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
@@ -5,19 +5,20 @@ import org.junit.Test;
 
 public class CommandLine extends TestCase {
 
-    @Test
-    public void testUnoJarAntFromGradleSmoke1() throws Exception {
-        Invoker.Result result = Invoker.run("java -jar build/testjar1.jar");
-        assertEquals("Expected failure did not occur: " + result, 0, result.status);
-        assertEquals(result.out.get(0),"System Out Success - main class");
-        assertEquals(result.err.get(0),"System Err Success - library class");
-    }
-    @Test
-    public void testUnoJarGradleTaskSmoke2() throws Exception {
-        Invoker.Result result = Invoker.run("java -jar build/testJar2.jar");
-        assertEquals("Expected failure did not occur: " + result, 0, result.status);
-        assertEquals(result.out.get(0),"System Out Success - main class");
-        assertEquals(result.err.get(0),"System Err Success - library class");
-    }
+  @Test
+  public void testUnoJarAntFromGradleSmoke1() throws Exception {
+    Invoker.Result result = Invoker.run("java -jar build/testjar1.jar");
+    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(result.out.get(0), "System Out Success - main class");
+    assertEquals(result.err.get(0), "System Err Success - library class");
+  }
+
+  @Test
+  public void testUnoJarGradleTaskSmoke2() throws Exception {
+    Invoker.Result result = Invoker.run("java -jar build/testJar2.jar");
+    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(result.out.get(0), "System Out Success - main class");
+    assertEquals(result.err.get(0), "System Err Success - library class");
+  }
 
 }
