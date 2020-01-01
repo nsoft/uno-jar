e3270d451b3cfb76fadd320a4d153d829ba1d523
nsoft <gus@needhamsoftware.com>
2020-01-01 16:58:11 -0500

    Remove more old stuff

--- tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
+++ tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
@@ -6,43 +6,18 @@ import org.junit.Test;
 public class CommandLine extends TestCase {
 
     @Test
-    public void testOneJarAntFromGradleSmoke1() throws Exception {
+    public void testUnoJarAntFromGradleSmoke1() throws Exception {
         Invoker.Result result = Invoker.run("java -jar build/testjar1.jar");
-        assertTrue("Expected failure did not occur: " + result, result.status == 0);
+        assertEquals("Expected failure did not occur: " + result, 0, result.status);
         assertEquals(result.out.get(0),"System Out Success - main class");
         assertEquals(result.err.get(0),"System Err Success - library class");
     }
     @Test
-    public void testOneJarGradleTaskSmoke2() throws Exception {
+    public void testUnoJarGradleTaskSmoke2() throws Exception {
         Invoker.Result result = Invoker.run("java -jar build/testJar2.jar");
-        assertTrue("Expected failure did not occur: " + result, result.status == 0);
+        assertEquals("Expected failure did not occur: " + result, 0, result.status);
         assertEquals(result.out.get(0),"System Out Success - main class");
         assertEquals(result.err.get(0),"System Err Success - library class");
     }
-//
-//    public void testOneJarSmoke2() throws Exception {
-//        Result result = Invoker.run("java -jar dist/one-jar-example-0.98.jar");
-//        assertTrue("Expected pass did not occur: " + result, result.status == 0);
-//    }
-//
-//    public void testOneJarMacroSmoke3() throws Exception {
-//        Result result = Invoker.run("java -jar dist/one-jar-example-macro-0.98.jar");
-//        assertTrue("Expected pass did not occur: " + result, result.status == 0);
-//    }
-//
-//    public void testOneJarTaskSmoke4() throws Exception {
-//        Result result = Invoker.run("java -jar dist/one-jar-example-task-0.98.jar");
-//        assertTrue("Expected pass did not occur: " + result, result.status == 0);
-//    }
-//
-//    public void testOneJarAlternate1() throws Exception {
-//        Result result = Invoker.run("java -classpath dist/one-jar-example-0.98.jar OneJar");
-//        assertTrue("Expected pass did not occur: " + result, result.status == 0);
-//    }
-//
-//    public void testOneJarExt() throws Exception {
-//        Result result = Invoker.run("java -classpath dist/one-jar-example-ext-0.98.jar OneJar");
-//        assertTrue("Expected pass did not occur: " + result, result.status == 0);
-//    }
 
 }
