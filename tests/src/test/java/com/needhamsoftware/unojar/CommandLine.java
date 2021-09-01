3a8ea4b28299cf81e0eaf3aefb60c082fe819e1c
Gus Heck <gus@needhamsoftware.com>
2021-09-01 10:44:30 -0400

    Fix incorrect message in texts
    (cherry picked from commit d55425b090210a5a9770ae594197989a88d3f982)

--- tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
+++ tests/src/test/java/com/needhamsoftware/unojar/CommandLine.java
@@ -4,11 +4,13 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class CommandLine extends TestCase {
+  // todo: probably should have some negative tests
+  public static final String TEST_SHOULD_NOT_ERROR = "Unexpected Failure - return code not zero: ";
 
   @Test
   public void testUnoJarAntFromGradleSmoke1() throws Exception {
     Invoker.Result result = Invoker.run("java -jar build/testjar1.jar");
-    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(TEST_SHOULD_NOT_ERROR + result, 0, result.status);
     assertEquals( "System Out Success - main class", result.out.get(0));
     assertEquals("System Err Success - library class", result.err.get(0));
   }
@@ -24,7 +26,7 @@ public class CommandLine extends TestCase {
   @Test
   public void testUnoJarGradleTaskSmokeLog4j() throws Exception {
     Invoker.Result result = Invoker.run("java -jar build/testLog4j.jar");
-    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(TEST_SHOULD_NOT_ERROR + result, 0, result.status);
     assertEquals( "TEST:  ERROR [main] TestMainLog4jPlugin   - Log4j Success - main class", result.out.get(0));
     assertEquals("TEST:  ERROR [main] LibTestLog4jPlugin   - Log4J Success - library class", result.out.get(1));
   }
@@ -32,7 +34,7 @@ public class CommandLine extends TestCase {
   @Test
   public void testUnoJarGradleTaskSmokeMRJar() throws Exception {
     Invoker.Result result = Invoker.run("java -jar  build/testMRJar.jar");
-    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(TEST_SHOULD_NOT_ERROR + result, 0, result.status);
     assertEquals( "TEST:  ERROR [main] TestMainLog4jMRJar   - Log4j Success - main class", result.out.get(0));
     assertEquals("TEST:  ERROR [main] LibTestLog4jPlugin   - Log4J Success - library class", result.out.get(1));
   }
@@ -41,6 +43,6 @@ public class CommandLine extends TestCase {
   public void testUnoJarGradleTaskDirWithSlash() throws Exception {
 //    Invoker.Result result = Invoker.run("java -agentlib:jdwp=transport=dt_socket,server=n,address=ns-l1:5006,suspend=y -jar  build/testDirWithSlashJar.jar");
     Invoker.Result result = Invoker.run("java -jar  build/testDirWithSlashJar.jar");
-    assertEquals("Expected failure did not occur: " + result, 0, result.status);
+    assertEquals(TEST_SHOULD_NOT_ERROR + result, 0, result.status);
   }
 }
