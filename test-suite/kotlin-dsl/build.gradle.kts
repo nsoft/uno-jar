173565f3c303994f01d6e48e9ea3803cd1de4800
nsoft <gus@needhamsoftware.com>
2023-01-18 19:40:40 -0500

    Fix broken writing of manifest main attributes, and make final jar name available via tasks.getByName("packageUnoJar").getArchiveFileName()

--- test-suite/kotlin-dsl/build.gradle.kts
+++ test-suite/kotlin-dsl/build.gradle.kts
@@ -12,21 +12,24 @@ java {
 }
 
 application {
-    mainClass.set("Main")
+    mainClass.set("MainK1")
 }
 
 dependencies {
-    implementation("io.javalin:javalin:3.13.10")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.32")
+}
+
+unojar {
+    manifestAttributes.set(mapOf("Test-Attribute" to "Kotlin-Extension"))
 }
 
 tasks {
     register<com.needhamsoftware.unojar.gradle.PackageUnoJarTask>("packageUnoJar2") {
         dependsOn("jar")
-        mainClass.set("Main2")
+        mainClass.set("MainK2")
         archiveBaseName.set("test")
         embedConfiguration.set(configurations.getByName("runtimeClasspath"))
-        manifestAttributes.set(mapOf("Test-Attribute" to "TestValue"))
+        manifestAttributes.set(mapOf("Test-Attribute" to "Kotlin-Register"))
     }
 }
 
--- test-suite/kotlin-dsl/src/main/java/MainK1.java
+++ test-suite/kotlin-dsl/src/main/java/MainK1.java
@@ -1,4 +1,5 @@
-public class Main2 {
+
+public class MainK1 {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
