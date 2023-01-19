7eb24602b26d2afe22c5840d44b9814163e98bf5
nsoft <gus@needhamsoftware.com>
2023-01-18 19:50:38 -0500

    Fix oops try with resources

--- test-suite/test-runner/src/test/java/GradleSpecificTests.java
+++ test-suite/test-runner/src/test/java/GradleSpecificTests.java
@@ -14,29 +14,30 @@ public class GradleSpecificTests {
 
   @Test
   public void testGroovyDslUsingExtension()
-          throws Exception {
+      throws Exception {
     testUnoJar("../groovy-dsl/build/libs/groovy-dsl-unojar.jar", "Groovy-Extension");
   }
+
   @Test
   public void testGroovyDslUsingTasksRegister()
-          throws Exception {
+      throws Exception {
     testUnoJar("../groovy-dsl/build/libs/test-unojar.jar", "Groovy-Register");
   }
 
   @Test
   public void testKotlinDslExtension()
-          throws Exception {
+      throws Exception {
     testUnoJar("../kotlin-dsl/build/libs/kotlin-dsl-unojar.jar", "Kotlin-Extension");
   }
 
   @Test
   public void testKotlinDslRegister()
-          throws Exception {
+      throws Exception {
     testUnoJar("../kotlin-dsl/build/libs/test-unojar.jar", "Kotlin-Register");
   }
 
   public void testUnoJar(String pathname, String testValue)
-          throws Exception {
+      throws Exception {
     final File unoJarFile = new File(pathname);
     assertTrue(unoJarFile.isFile());
     final Invoker.Result result = Invoker.run(String.format("java -jar %s", unoJarFile));
@@ -47,21 +48,23 @@ public class GradleSpecificTests {
       ZipEntry entry = zf.getEntry("META-INF/MANIFEST.MF");
       assertNotNull(entry);
       inputStream = zf.getInputStream(entry);
-    }
-    assertNotNull(inputStream);
-    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-    String line;
-    boolean found = false;
-    while (reader.ready() && ! found) {
-      line = reader.readLine();
-      String[] split = line.split(":");
-      if (split.length == 2) {
-        if ("Test-Attribute".equals(split[0].trim())) {
-          found = testValue.equals(split[1].trim());
+
+      assertNotNull(inputStream);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+      String line;
+      boolean found = false;
+      while (reader.ready() && !found) {
+        line = reader.readLine();
+        String[] split = line.split(":");
+        if (split.length == 2) {
+          if ("Test-Attribute".equals(split[0].trim())) {
+            found = testValue.equals(split[1].trim());
+          }
         }
       }
+
+      assertTrue("Test-Attribute not found in Manifest", found);
     }
-    assertTrue("Test-Attribute not found in Manifest", found);
   }
 
 }
